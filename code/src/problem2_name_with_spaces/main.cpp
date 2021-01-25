e1bb0c6e40acabddf3702d079f65789359bbc22c
Georgii Zhulikov <l8doku@gmail.com>
2021-01-25 21:35:53 +0300

    Update template

--- code/src/problem2_name_with_spaces/main.cpp
+++ code/src/problem2_name_with_spaces/main.cpp
@@ -1,9 +1,18 @@
-/*******************************************************************************
- *
- * Modify example 1, so it allows a user to input a complex name that
- * consists of more than 1 word (separated by spaces).
- *
- ******************************************************************************/
+////////////////////////////////////////////////////////////////////////////////
+/// \file
+/// \brief      Main module for Problem 2: Name with spaces.
+/// \author     Georgii Zhulikov
+/// \author     Sergey Shershakov
+/// \version    0.1.0
+/// \date       25.01.2021
+///             This code is for educational purposes of the course "Introduction
+///             to programming" provided by the Faculty of Computer Science
+///             at the Higher School of Economics.
+///
+/// Modify example 1, so it allows a user to input a complex name that
+/// consists of more than 1 word (separated by spaces).
+///
+////////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include <string>
